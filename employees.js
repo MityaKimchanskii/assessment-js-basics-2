223f5260aa923d3b4ce3aed8ece2c17a937478d2
Mitya Kim <mitya_kim@icloud.com>
2021-11-19 11:55:28 -0800

    Emploees commit

--- employees.js
+++ employees.js
@@ -20,7 +20,15 @@
 */
 
 //CODE HERE
-
+class Employee {
+    constructor(name, shifts){
+        this.name = name
+        this.shifts = shifts
+    }
+    getSchedule(){
+        console.log(`${this.name} works on ${this.shifts}`)
+    }
+}
 
 
 /*
@@ -34,6 +42,7 @@
 */
 
 //CODE HERE
+let empOne = new Employee('Tom', 'weekday mornings, weekday afternoons')
 
 /*
     Call the `getSchedule` method on the
@@ -41,7 +50,7 @@
 */
 
 //CODE HERE
-
+empOne.getSchedule()
 
 /*
     Make a copy of the empOne object
@@ -56,7 +65,9 @@
 */
 
 //CODE HERE
-
+const empTwo = {...empOne}
+empTwo.name = 'Nick'
+console.log(empTwo.name)
 
 
 //////////////////PROBLEM 2////////////////////
@@ -83,7 +94,18 @@
 */
 
 //CODE HERE
-
+class Manager extends Employee{
+    constructor(name, shifts, employees){
+        super(name,shifts)
+        this.employees = employees
+    }
+    getEmployees(){
+        console.log(`${this.name} manages ${this.employees}`)
+    }
+    addEmployee(emp){
+        this.employees.push(emp)
+    }
+}
 
 
 /*
@@ -98,7 +120,7 @@
 */
 
 //CODE HERE
-
+const manager = new Manager('Winston', 'weekday mornings, weekday afternoons', ['Cece', 'Schmidt'])
 
 /*
     Call the `getEmployees` method on the
@@ -106,6 +128,7 @@
 */
 
 //CODE HERE
+manager.getEmployees()
 
 /*
     Call the `addEmployee` method on the 
@@ -114,7 +137,7 @@
 */
 
 //CODE HERE 
-
+manager.addEmployee('Sam')
 /*
     Call the `getEmployees` method on the
     `manager` object again to confirm 
@@ -122,3 +145,4 @@
 */
 
 //CODE HERE
+manager.getEmployees()
